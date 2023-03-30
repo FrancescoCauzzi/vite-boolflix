ef57b13c401e917a06bda06fd309f12eaf6d46fc
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-30 20:24:05 +0200

    feat: tentativi vari bandiere

--- src/components/BflixMainFolder/BflixMainCard.vue
+++ src/components/BflixMainFolder/BflixMainCard.vue
@@ -13,6 +13,24 @@ export default {
     origLang: String,
     rating: Number,
   },
+  computed: {
+    flagUrl() {
+      const countryCode = this.getCountryCode(this.origLang);
+      console.log(countryCode);
+      return `https://flagcdn.com/w40/${countryCode}.png`;
+    },
+  },
+  methods: {
+    async getCountryCode(languageCode) {
+      const response = await fetch(
+        `https://restcountries.com/v3.1/lang/${languageCode}`
+      );
+      const data = await response.json();
+      console.log(data);
+      const countryCode = data[0]?.cca2;
+      return countryCode || "unknown";
+    },
+  },
 };
 </script>
 <template>
@@ -22,6 +40,7 @@ export default {
         <h5 class="__card-title">Title: {{ title }}</h5>
         <h5 class="__card-orig-title">Original title: {{ origTitle }}</h5>
         <h6 class="__card-orig-lang">Original language: {{ origLang }}</h6>
+        <img :src="flagUrl" alt="Language flag" />
         <h6 class="__card-rate">Rating: {{ rating }}</h6>
       </div>
     </div>
